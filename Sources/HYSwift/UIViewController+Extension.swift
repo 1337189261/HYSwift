9f671814fadb82042e66f4d33a5bfe6b20a6e64d
Haoyu Chen <chy66588@hotmail.com>
2020-06-24 11:22:31 +0800

    add vc functions

--- Sources/HYSwift/UIViewController+Extension.swift
+++ Sources/HYSwift/UIViewController+Extension.swift
@@ -24,4 +24,29 @@ extension UIViewController {
     public var bottombarHeight: CGFloat {
         return tabBarController?.view.frame.height ?? 0
     }
+    
+    class func topMostViewController(base: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
+
+        if let nav = base as? UINavigationController {
+            return topMostViewController(base: nav.visibleViewController)
+        }
+
+        if let tab = base as? UITabBarController {
+            let moreNavigationController = tab.moreNavigationController
+
+            if let top = moreNavigationController.topViewController, top.view.window != nil {
+                return topMostViewController(base: top)
+            } else if let selected = tab.selectedViewController {
+                return topMostViewController(base: selected)
+            }
+        }
+        if let presented = base?.presentedViewController {
+            return topMostViewController(base: presented)
+        }
+        return base
+    }
+    
+    class var navController: UINavigationController? {
+        return UIApplication.shared.keyWindow?.rootViewController?.navigationController
+    }
 }
